257f649b62058f400762f4cf72b7885b783a188f
Keegan Ruebling and Margaret Berry <keegan@>
2017-10-31 11:55:05 -0700

    add kitty stories

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -11,7 +11,7 @@ export class StoryComponent {
   story: Story[] = [
     new Story(1, "Kitteh haz target of keyz on table. Should kitteh knock off?", "Owner waz lookin v hard for keyz all along and heard the keyz do a drop. He praises kitteh for hiz effortz with head rubz", "Owner doin an angry cause he cant find his keyz even though they're sittin on the table. He lashes out at kitteh in frustration."),
     new Story(2, "Kitteh caught a whiff of smellz comin from up high place. Should kitteh do a leap to investigate?", "Kitteh doez a resist and doesnt do a leap. Owner commendz her impeccable self-discipline with head rubz", "Kitteh doez a leap and haz her paw-bean land on a v hot stove. Oh noez! Kitteh scamperz off to mend its woondz."),
-    new Story(3,)
+    new Story(3, "Kitteh claws feelin stressed and underused. While doin a prowl around ze house, kitteh spots favorite corner of favorite sofa. Should kitteh do a scrape-scrape on most favorite spot or on kitteh old, overused, scratchin post?", "Kitteh doez a flex-reach and does a scrape-scrape on most favorite spot. Kitteh purr boxx roars contentedly. Human male owner does a peek around the corner at now ruined sofa. Human female owner scoops kitteh up and gives a rub. Human female haz voice sounds of 'hate' and 'couch' and 'new one anyway'.", "Kitteh doez a walk to old scrape-scrape spot, and doez a stretch to do a scratch. After a couple scrape-scrapes kitteh notice pain in paw-bean. Kitteh doez a meow! Kitteh haz splinter!")
   ]
 
 }
